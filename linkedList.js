d0a47f3d43917b1f9ec0121dbde5db1a5836da9f
Beppe <giuseppe.monorchio94@hotmail.it>
2023-06-11 17:19:14 +0200

    add function to remove the last element in the list and to check if a given element is in the list or not

--- linkedList.js
+++ linkedList.js
@@ -13,6 +13,8 @@ class LinkedList{
     }
 
     append(data) {
+
+        //append a new node to the end of the list
         const newNode = new Node(data);
 
         if(!this.head) {
@@ -25,6 +27,8 @@ class LinkedList{
     }
 
     prepend(data) {
+
+        //prepend a new node to the start of the list
         const newNode = new Node(data);
         const head = this.head;
 
@@ -39,6 +43,8 @@ class LinkedList{
     }
 
     size() {
+
+        //return the sum of elements in the list
         let current = this.head;
         let counter = 0;
 
@@ -47,18 +53,24 @@ class LinkedList{
             counter++;
         }
 
-        return `This list is ${counter} elements`;
+        return `This list is ${counter} elements long`;
     }
 
     firstElement() {
+
+        //return the first element in the list
         return `The first element is ${this.head.value}`;
     }
 
     lastElement() {
+
+        //return last element in the list
         return `The last element is ${this.tail.value}`;
     }
 
     elementAt(index) {
+
+        //return the node at given index
         let current = this.head;
 
         for (let i = 0; i < index; i++) {
@@ -69,20 +81,61 @@ class LinkedList{
 
         return current;
     }
+    
+    removeElement() {
+
+        //if list is empty, return
+        if(!this.head) {
+            return;
+        }
+        
+        //if there is only one element to remove in the list 
+        if(this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+            return;
+        }
+        
+        //check if the next node is the tail, if so remove it
+        let current = this.head;
+        while(current.next !== this.tail) {
+            current = current.next;
+        }
+
+        current.next = null;
+        this.tail = current;
+    }
+
+    contains(element) {
+        
+        //check if given value is in the list, if so return true else false
+        let current = this.head;
+
+        while(current !== null) {
+
+            if(current.value === element) return `This element is in the list`;
+                        
+            current = current.next;
+        }
+
+        return `This element is not in the list`;
+    }
 }
 
 
 const list = new LinkedList();
 
+list.prepend(5)
 list.append(10);
 list.append(20);
 list.append(25);
-list.prepend(5);
-list.prepend(9);
 list.append(24);
+
+list.removeElement()
 
 console.log(list.size());
 console.log(list);
 console.log(list.firstElement());
 console.log(list.lastElement());
-console.log(list.elementAt(5));
+console.log(list.elementAt(2));
+console.log(list.contains(0));
