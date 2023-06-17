55bfa9e6b7f51975b71a06927e36074703043953
Beppe <giuseppe.monorchio94@hotmail.it>
2023-06-17 18:00:20 +0200

    add funcgion to remove a node at given index and a function to insert a new node at given index

--- linkedList.js
+++ linkedList.js
@@ -53,7 +53,7 @@ class LinkedList{
             counter++;
         }
 
-        return `This list is ${counter} elements long`;
+        return counter;
     }
 
     firstElement() {
@@ -76,7 +76,7 @@ class LinkedList{
         for (let i = 0; i < index; i++) {
 
             current = current.next;
-            if (current === null) return "There is no item for this index";
+            if (current === null) return "There is no item at this index";
         }
 
         return current;
@@ -123,35 +123,137 @@ class LinkedList{
 
     find(element) {
 
+        //initialize 2 variables with the current head of the list and a counter set to 0 (index)
         let current = this.head;
         let counter = 0;
 
+        //iterate over list 
         while(current != null) {
             
+            //check if given value is equal to the current value being checked
             if(current.value === element) {
+
+                //if it is, return counter variable which is the index of the value
                 return `The element is at index ${counter}`;
-            }   
+            }
+
+            //move to the next node and increment counter variable
             current = current.next;
             counter++;
         }
         
+        //if the element is not found return
         return `Element not in the list`;;
     }
 
     toString() {
 
+        //initialize 2 variables with the current head of the list and an empty string
         let current = this.head;
         let string = '';
         
+        //iterate over the list 
         while(current !== null) {
+
+            //concatenate strings to show all the elemenets in the list 
             string += `(${current.value}) -> `;
 
+            //move current node to the next
             current = current.next;
         }
         
+        //concatenate null to the altrady finished string
         return string + 'null';
     }
+
+    removeAt(index) {
+        
+        //initialize 3 variables with the current head node, 
+        //the previous node set to null
+        //and the current index counter set to 0
+        let current = this.head;
+        let previous = null;
+        let currIndex = 0;
+
+        //check if the given index is less than 0 or greater than list length
+        if(index < 0 || index > this.size()) return 'Wrong index input';
+
+        //check if the given index is the first element in the list and update the head
+        //and check if that was the only element
+        //if it is update the tail node
+        if(index === 0) {
+            this.head = this.head.next;
+            if(!this.head) {
+                this.tail = null;
+            }
+            return
+        }
+
+        //iterate over the list updating the initial variables
+        while(current && currIndex < index) {
+            previous = current;
+            current = current.next;
+            currIndex++;
+        }
+
+        if(current) {
+            //remove the node by updating the reference 
+            previous.next = current.next;
+
+            //update tail node if remove last node
+            if(current === this.tail) {
+                this.tail = previous;
+            }
+        }
+    }
+
+    insertAt(value, index) {
+
+        //initialize 3 variables with the current head node, 
+        //the previous node set to null
+        //and the current index counter set to 0
+        let current = this.head;
+        let previous = null;
+        let currIndex = 0;
+
+        //check if the given index is less than 0 or greater than list length
+        if(index < 0 || index > this.size()) return 'Wrong index input';
+
+        //check if the given index is 0 (first element in the list) 
+        //if it is we update the current head and the next node
+        if(index === 0) {
+            let newNode = new Node(value);
+            newNode.next = this.head;
+            this.head = newNode;
+            //if the lits is empty, updte the tail node
+            if(!this.tail) {
+                this.tail = newNode;
+            }
+        } else {
+
+            //finde the given index in the list
+            //and update initial variables
+            while(current && currIndex < index) {
+                previous = current;
+                current = current.next;
+                currIndex++;
+            }
+
+            //insert the new node once the index is found
+            if(currIndex === index) {
+                let newNode = new Node(value);
+                newNode.next = current;
+                previous.next = newNode;
+                //if inserting at the end, update tail node
+                if(!current) {
+                    this.tail = newNode;
+                }
+            }
+        }
+    }
 }
+
+
 
 
 const list = new LinkedList();
@@ -173,4 +275,8 @@ console.log(list.lastElement());
 console.log(list.elementAt(2));
 console.log(list.contains(0));
 console.log(list.find(20));
+console.log(list.toString());
+console.log(list.removeAt(1));
+console.log(list.toString());
+console.log(list.insertAt(100, 0));
 console.log(list.toString());
